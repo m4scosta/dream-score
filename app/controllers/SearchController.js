86fb14dafef8e120032795ad70a55f01b7e07efd
Leonardo Borges Avelino <lborgav@gmail.com>
2015-09-19 05:41:49 -0300

    Modificação do Search

--- app/controllers/SearchController.js
+++ app/controllers/SearchController.js
@@ -19,6 +19,7 @@ angular
    */
   function SearchController($scope, $http, $routeParams, uiGmapGoogleMapApi) {
     $scope.loc = $routeParams.location;
+
     var initialCenter = {
       latitude: -23.5534084,
       longitude: -46.6577078
@@ -34,31 +35,42 @@ angular
         };
         $scope.map = { center: initialCenter, zoom: 13 };
         $scope.circles = [
-      {
-        id: 0,
-        center: initialCenter,
-        radius: 2000,
-        stroke: {
-            color: '#08B21F',
-            weight: 2,
-            opacity: 1
-        },
-        fill: {
-            color: '#08B21F',
-            opacity: 0.1
-        },
-        geodesic: true,
-        draggable: true,
-        clickable: true,
-        editable: true,
-        visible: true,
-        control: {}
-      }
-    ];
+          {
+            id: 0,
+            center: { latitude: lat,
+          longitude: lng},
+            radius: 2500,
+            stroke: {
+                color: '#08B21F',
+                weight: 2,
+                opacity: 1
+            },
+            fill: {
+                color: '#08B21F',
+                opacity: 0.1
+            },
+            geodesic: true,
+            draggable: true,
+            clickable: true,
+            editable: true,
+            visible: true,
+            control: {}
+          }
+        ];
 
       }
     );
 
+    $scope.hoverIn = function(id) {
+      var el = $scope.markers[id];
+      $scope.map = { center: {latitude: el.coords.latitude, longitude: el.coords.longitude }, zoom: 13 };
+      el.options.animation = '1';
+    }
+
+    $scope.hoverOut = function(id) {
+      var el = $scope.markers[id];
+      el.options.animation = '0';
+    }
 
     $scope.applyFilters= function() {
       $scope.markers = [
@@ -70,8 +82,8 @@ angular
           },
           options: {
             draggable: false,
-            labelContent: "R$ 100,00",
-            labelAnchor: "30 70",
+            labelContent: "R$100",
+            labelAnchor: "20 70",
             labelClass: "marker-labels"
           }
         },
@@ -81,6 +93,12 @@ angular
             latitude: -23.5634084,
             longitude: -46.6577078
           },
+          options: {
+            draggable: false,
+            labelContent: "R$100",
+            labelAnchor: "20 70",
+            labelClass: "marker-labels"
+          }
         },
         {
           id: 2,
@@ -88,6 +106,12 @@ angular
             latitude: -23.5434084,
             longitude: -46.6577078
           },
+          options: {
+            draggable: false,
+            labelContent: "R$100",
+            labelAnchor: "20 70",
+            labelClass: "marker-labels"
+          }
         }
       ];
     };
